a5fa5c85a816f3f0f088cdb4466bf8960c9366d2
Jaakob Alakulppi <jbalakulppi@gmail.com>
2024-11-11 17:13:33 -0500

    added localhost back to sequelize

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -11,7 +11,7 @@ const sequelize = process.env.DB_URL
       process.env.DB_USER || '',
       process.env.DB_PASSWORD,
       {
-        // host: 'localhost',
+        host: 'localhost',
         dialect: 'postgres',
         dialectOptions: {
           decimalNumbers: true,
