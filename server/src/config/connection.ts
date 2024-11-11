4dbd50425f6f062d24a5a8c20769d893f0467328
Jaakob Alakulppi <jbalakulppi@gmail.com>
2024-11-11 14:45:17 -0500

    removed frontend url

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -4,49 +4,51 @@ dotenv.config();
 import { Sequelize } from 'sequelize';
 
 // ORIGINAL SEQUELIZE
-// const sequelize = process.env.DB_URL
-//   ? new Sequelize(process.env.DB_URL)
-//   : new Sequelize(
-//       process.env.DB_NAME || '',
-//       process.env.DB_USER || '',
-//       process.env.DB_PASSWORD,
-//       {
-//         host: 'localhost',
-//         dialect: 'postgres',
-//         dialectOptions: {
-//           decimalNumbers: true,
-//           charset: 'utf8',
-//         },
-//       }
-//     );
+const sequelize = process.env.DB_URL
+  ? new Sequelize(process.env.DB_URL)
+  : new Sequelize(
+      process.env.DB_NAME || '',
+      process.env.DB_USER || '',
+      process.env.DB_PASSWORD,
+      {
+        host: 'localhost',
+        dialect: 'postgres',
+        dialectOptions: {
+          decimalNumbers: true,
+          charset: 'utf8',
+        },
+      }
+    );
 
 console.log("DB Host:", process.env.DB_HOST);
 console.log("DB Port:", process.env.DB_PORT);
 console.log("DB User:", process.env.DB_USER);
 console.log("DB Name:", process.env.DB_NAME);
 console.log("DB Password:", process.env.DB_PASSWORD);
+console.log("DB url:", process.env.DB_URL);
 
 
-const dbUrl = process.env.DB_HOST!;
-const { host, pathname, username, password } = new URL(dbUrl);
 
-const sequelize = new Sequelize(
-  pathname?.slice(1) || '', // Extract the DB name by removing the leading '/'
-  username || '',
-  password || '',
-  {
-    host: host || '',
-    port: parseInt(process.env.DB_PORT || '5432', 10), // Ensure port is set or defaults to 5432
-    dialect: 'postgres',
-    logging: false, // Set to true to log queries for debugging
-    dialectOptions: {
-      ssl: {
-        require: true, // Ensure SSL is required for Render
-        rejectUnauthorized: false, // Allow self-signed SSL certs (necessary for Render)
-      },
-    },
-  }
-);
+// const dbUrl = process.env.DB_HOST!;
+// const { host, pathname, username, password } = new URL(dbUrl);
+
+// const sequelize = new Sequelize(
+//   pathname?.slice(1) || '', // Extract the DB name by removing the leading '/'
+//   username || '',
+//   password || '',
+//   {
+//     host: host || '',
+//     port: parseInt(process.env.DB_PORT || '5432', 10), // Ensure port is set or defaults to 5432
+//     dialect: 'postgres',
+//     logging: false, // Set to true to log queries for debugging
+//     dialectOptions: {
+//       ssl: {
+//         require: true, // Ensure SSL is required for Render
+//         rejectUnauthorized: false, // Allow self-signed SSL certs (necessary for Render)
+//       },
+//     },
+//   }
+// );
 
 // Test connection to the database
 sequelize.authenticate()
--- server/src/server.ts
+++ server/src/server.ts
@@ -16,7 +16,7 @@ const port = 3001;
 const forceDatabaseRefresh = false;
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:3000",
+  origin: "http://localhost:3000",
   credentials: true, 
 }));
 
