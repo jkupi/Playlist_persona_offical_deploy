a3aed115c91a5ff7f72815cf3a9ace0b70f6dc0c
Jaakob Alakulppi <jbalakulppi@gmail.com>
2024-11-11 14:10:21 -0500

    changed ports

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,16 +4,16 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 10000,
+    port: 3000,
     open: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'http://localhost:10000',
         changeOrigin: true,
         secure: false,
       },
       '/auth': {
-        target: 'http://localhost:3001',
+        target: 'http://localhost:10000',
         changeOrigin: true,
         secure: false,
       },
