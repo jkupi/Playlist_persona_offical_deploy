f8e2fb5421baf2a9cd89ec92adf5bedc49ac39ca
Jaakob Alakulppi <jbalakulppi@gmail.com>
2024-11-11 18:25:42 -0500

    added route for landing page

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -4,7 +4,9 @@ import authRoutes from './auth-routes.js';
 // import { authenticateToken } from '../middleware/auth.js';
 const router = Router();
 
-
+router.get("/", (_req, res) => {
+    res.send("welcome to the playlist persona");
+});
 router.use('/auth', authRoutes);
 router.use('/api', apiRoutes);
 
