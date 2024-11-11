1d2588b57a77ff7e72ef3a5ad58ea8be4042970b
Jaakob Alakulppi <jbalakulppi@gmail.com>
2024-11-11 18:31:35 -0500

    added .get to index.html

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -2,11 +2,18 @@ import { Router } from 'express';
 import apiRoutes from './api/index.js';
 import authRoutes from './auth-routes.js';
 // import { authenticateToken } from '../middleware/auth.js';
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const router = Router();
 
 router.get("/", (_req, res) => {
-    res.send("welcome to the playlist persona");
+    res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
 });
+
 router.use('/auth', authRoutes);
 router.use('/api', apiRoutes);
 
